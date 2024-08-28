8e4a9469a5d8ff6ebc3a3688674f3cceaedf51ed
Saboor Ahmad <saboorahm7e@gmail.com>
2024-08-29 00:56:22 +0500

    Initial Commit

---
+++ JoinVideo/JoinVideoApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoinVideoApp.swift
+//  JoinVideo
+//
+//  Created by Saboor on 29/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct JoinVideoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
